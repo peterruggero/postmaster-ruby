2e4f2286d79a6ad526b7b4407277c9c230fc489f
Wojciech Lichota <wojciech@lichota.pl>
2013-06-26 15:38:36 +0200

    return id as integer

--- lib/postmaster/package.rb
+++ lib/postmaster/package.rb
@@ -1,8 +1,12 @@
 module Postmaster
 
   class Package < APIResource
-    include Postmaster::APIOperations::Create
     include Postmaster::APIOperations::List
+
+    def self.create(params={})
+      response = Postmaster.request(:post, self.url, params)
+      response[:id]
+    end
 
     def self.fit(params={})
       json_params = Postmaster::JSON.dump(params, :pretty => true)
